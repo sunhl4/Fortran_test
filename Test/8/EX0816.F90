36a3ffb82e5ad725ee7b2b4603d0a9bdfa388fea
sunhl4 <15051962326@163.com>
2023-08-08 13:40:40 +0800

    new

---
+++ Test/8/EX0816.F90
@@ -0,0 +1,38 @@
+program ex0816
+implicit none
+    integer :: a(5) = (/ 1,2,3,4,5 /)
+    call ShowOne(a(1))
+    call ShowArray5(a)
+    call ShowArray3(a)
+    call ShowArray3(a(2))
+    call ShowArray2X2(a)
+    stop
+end
+
+subroutine ShowOne(num)
+implicit none
+    integer :: num 
+    write(*,*)num
+    return
+end
+
+subroutine ShowArray5(num)
+implicit none
+    integer :: num(5)
+    write(*,*) num
+    return 
+end
+
+subroutine ShowArray3(num)
+implicit none
+    integer :: num(3)
+    write(*,*)num
+    return
+end
+
+subroutine ShowArray2X2(num)
+implicit none
+    integer :: num(2,2)
+    write(*,*) num(2,1), num(2,2)
+    return
+end
