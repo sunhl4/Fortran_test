a62d352a88d48373e24d54dee9312a77d3f0a347
sunhl4 <15051962326@163.com>
2023-08-17 22:41:06 +0800

    new

--- Test/9/EX0902.F90
+++ Test/9/EX0902.F90
@@ -3,7 +3,7 @@ implicit none
     character(len=20) :: string
     open(unit=10,file="test.file")
     write(10,"(A20)")"Good morning."
-    rewind(10) !会把“10”文件读写位置移到最前边
+    rewind(10) !会把“10”文件读写位置移到最前边,endfile命令会把文件读写位置变成文件的结尾
     read(10,"(A20)") string
     write(*,*)string
     stop
