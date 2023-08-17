8e438abd6f0387013a9de7ee7796c47e43faea1d
sunhl4 <15051962326@163.com>
2023-08-17 22:37:07 +0800

    new

---
+++ Test/9/EX0903.F90
@@ -0,0 +1,16 @@
+program ex0903
+implicit none
+    character(len=20) :: filename = "ex0903.f90"
+    logical alive
+    inquire(file = filename, exist = alive) 
+    ! inquire 查询filename字符串内包含的文件名是否存在
+    ! 如果存在则alive的值为.TURE.
+
+    if (alive) then
+        write(*,*) filename, " exist."
+    else
+        write(*,*) filename, " doesn't exist."
+    end if 
+    stop
+end
+
