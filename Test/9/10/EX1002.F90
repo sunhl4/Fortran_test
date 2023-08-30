9573f743bd6d5d8ca3436d658ea59c6f2d06d628
sunhl4 <15051962326@163.com>
2023-08-30 21:11:48 +0800

    new

--- Test/9/10/EX1002.F90
+++ Test/9/10/EX1002.F90
@@ -1,10 +1,22 @@
 program ex012
 
     implicit none
-    integer, pointer :: p
-    allocate(p)
-    p = 100
-    write(*,*)p
-    stop
+    ! integer, pointer :: p
+    ! allocate(p)
+    ! p = 100
+    ! write(*,*)p
+    ! stop
+    character(len = 79) :: min
+    character(len = 79) :: spr
+    character(len = 79) :: pur 
+    min = "mineral water is: 矿泉水"
+    spr = "spring water is also: 矿泉水"
+    pur = "purified water is : 纯净水"
+    write(*, fmt=*) min
+    write(*, fmt=*) spr
+    write(*, fmt=*) pur
+    
+    
+
 
 end program ex012
